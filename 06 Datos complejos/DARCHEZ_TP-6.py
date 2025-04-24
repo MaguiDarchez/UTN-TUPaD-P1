bf4c976b3d21864dd155911c88e946e13f349d82
Magdalena Darchez <magdalenadarchez@gmail.com>
2025-04-24 18:31:17 -0300

    Subo el TP de Listas

---
+++ 06 Datos complejos/DARCHEZ_TP-6.py
@@ -0,0 +1,60 @@
+# print("-----Ejercicio 1-----")
+# mult_cuatro = list(range(4, 101, 4))
+# print(mult_cuatro)
+
+# print("-----Ejercicio 2-----")
+# elementos = ["Verde", "Violeta", "Azul", "Rosa", "Naranja"]
+# print(elementos[-2])
+
+# print("-----Ejercicio 3-----")
+# vacia = []
+# vacia.append("Hola")
+# vacia.append("cómo")
+# vacia.append("estás")
+# print(vacia)
+
+# print("-----Ejercicio 4-----")
+# animales = ["perro", "gato", "conejo", "pez"]
+# animales[-1] = "oso"
+# animales[-3] = "loro"
+# print(animales)
+
+# print("-----Ejercicio 5-----")
+# numeros = [8, 15, 3, 22, 7]
+# numeros.remove(max(numeros))
+# print(numeros)
+# print("Lo que hace el programa es crear una lista llamada numeros, " \
+# "con los valores 8, 15, 3, 22 y 7. Luego, elimina el maximo valor de" \
+# "esos numeros, en este caso 22. Al final lo imprime en la consola.")
+
+# print("-----Ejercicio 6-----")
+# numeros = list(range(10, 31, 5))
+# print(numeros[:2])
+
+# print("-----Ejercicio 7-----")  
+# autos = ["sedan", "polo", "suran", "gol"]
+# autos[1] = "mustang"
+# autos[2] = "camaro"
+# print(autos)
+
+# print("-----Ejercicio 8-----")
+# dobles = []
+# dobles.append(5*2)
+# dobles.append(10*2)
+# dobles.append(15*2)
+# print(dobles)
+
+# print("-----Ejercicio 9-----")
+# compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+# print(compras)
+# print("--(a)--")
+# compras[2].append("jugo")
+# print("--(b)--")
+# compras[1][1] = "tallarines"
+# print("--c--")
+# compras[0].remove("pan")
+# print(compras)
+
+# print("-----Ejercicio 10-----")
+# lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+# print(lista_anidada)
